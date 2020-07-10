8f3b57bd52f34536ac761907fcffc7cca6ae85b9
PratikshaJain37 <pratikshajain123@gmail.com>
2020-07-10 11:57:12 +0000

    Deployed successfully

--- basics/settings.py
+++ basics/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ty0b2*0uu#^%*&al314m*skskqs&izfrej9(&gf&yjf938uyal'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pratikshajain37.pythonanywhere.com']
 
 
 # Application definition
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = '/home/PratikshaJain37/django-notes/static/'
